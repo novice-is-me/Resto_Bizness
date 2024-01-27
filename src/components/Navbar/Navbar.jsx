42d9a8bfd7f23392b70fd8ff65f3831f35a224ec
Regina Jalandoni <reginajalandoni13@gmail.com>
2024-01-27 21:42:04 +0800

    First Commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+     <div className='navbar navbar-expand-lg p-4'>
+        <div className='container-fluid'>
+            <div>
+                <img src="" alt="" /> 
+                <a href="">Resto-Bizness</a> 
+            </div>
+        </div>
+     </div>
+    </>
+  )
+}
+
+export default Navbar
