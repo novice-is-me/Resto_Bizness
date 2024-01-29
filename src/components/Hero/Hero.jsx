97a4551a4f9ec6d845e184bd0347eafd6cf41c34
Regina Jalandoni <reginajalandoni13@gmail.com>
2024-01-29 22:15:13 +0800

    Updating Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import './hero.css' 
+import ButtonComponent from '../ButtonComponent' 
 import imageHero from '../../assets/imageHero.png'
 
 const Hero = () => {
   return (
     <div className='hero m-0 p-5'>
-      <div className='container d-flex'>
-        <div className='left-container'>
-            <h1>Explore Order and <br /> Savor</h1>
-            <p>Elevate your order experience with a simple click!</p>
+      <div className='container d-flex justify-content-between'>
+        <div className='d-flex left-container justify-content-center'>
+            <h1>EXPLORE ORDER AND <br /> SAVOR!</h1>
+            <p style={{fontWeight:'bold'}}>Elevate your order experience with a simple click!</p> 
+            <p>Delight in a seamless process as you discover, order, and savor with ease.</p>
+            <div>
+              <ButtonComponent/> 
+            </div>
         </div>
         <div className='right-container'>
             <img src={imageHero} alt="" />
