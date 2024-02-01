6ac1eb13c9bc47863052fe69fe88157e9e1d5d36
Regina Jalandoni <reginajalandoni13@gmail.com>
2024-02-01 22:22:03 +0800

    Updating About Section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './about.css'
+import CardComponent from '../CardComponent/CardComponent'
 
 const About = () => {
   return (
     <div className='about m-0 p-5'>
-      About
+        <div className='container'>
+            <CardComponent title={"Master Chefs"} text={"Good Quality"}/>
+            <CardComponent title={"Fast Delivery"} text={"Fast Delivery"}/>
+            <CardComponent title={"Best Quality"} text={"Best Quality"}/>
+            <CardComponent title={"Best Quality"} text={"Best Quality"}/>
+        </div>
     </div>
   )
 }
--- src/components/CardComponent/CardComponent.jsx
+++ src/components/CardComponent/CardComponent.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import './card.css'
 
-const CardComponent = () => {
+const CardComponent = ({img, title, text}) => {
   return (
-    <div>
-      
+    <div className='card'>
+        <img src={img} alt="" className='card-img-top' />
+        <div className='card-body'>
+            <h5 className='card-title'>{title}</h5>
+            <p className='card-text'>{text}</p>
+        </div>
     </div>
   )
 }
