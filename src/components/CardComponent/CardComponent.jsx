e6cdddccbcd89a94edb1eaa438d7dae5dfc2e418
Regina Jalandoni <reginajalandoni13@gmail.com>
2024-02-02 23:59:51 +0800

    Adding Hover Effects

--- src/components/CardComponent/CardComponent.jsx
+++ src/components/CardComponent/CardComponent.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './card.css'
 
 const CardComponent = ({image, title, text}) => {
+
+  const changeBackgroundEnter= (e) =>{
+    e.currentTarget.style.background = '#FF5F00';
+    e.currentTarget.querySelector('.card-title').style.color = 'white'
+    e.currentTarget.querySelector('.card-text').style.color = 'white'
+
+  }
+
+  const changeBackgroundLeave= (e) =>{
+    e.currentTarget.style.background = 'white';
+    e.currentTarget.querySelector('.card-title').style.color = 'black'
+    e.currentTarget.querySelector('.card-text').style.color = 'black'
+   
+  }
+
   return (
-    <div className='card col-lg col-md-4 p-3 me-3 mb-5'>
+    <div className='card col-lg col-md-4 p-3 me-3 mb-5' 
+    onMouseEnter={changeBackgroundEnter}
+    onMouseLeave={changeBackgroundLeave}>
         <img src={image} alt="" className='card-img-top' />
         <div className='card-body pt-3 p-0'> 
             <h5 className='card-title'>{title}</h5>
