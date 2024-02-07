2e93036d7190fb5f88e8fcdbfb845f9c9f7ac34e
Regina Jalandoni <reginajalandoni13@gmail.com>
2024-02-07 23:12:09 +0800

    Updating menucard

--- src/components/MenuCard/MenuCard.jsx
+++ src/components/MenuCard/MenuCard.jsx
@@ -6,13 +6,16 @@ const MenuCard = () => {
     <div className='menuCard m-0 p-5'>
       <div className='row'>
             {breakfastData.map((item, index) => {
-                return <div className='col col-lg-6 col-md-12'> 
-                     <div>
+                return <div className='col col-lg-6 col-md-12 d-flex align-items-center'> 
+                     <div className='image w-25'>
                          <img src={item.img} alt="" className='img-fluid' />
                      </div>
-                    <div>
-                         <h3>{item.title}</h3>
-                         <p>{item.description}</p>
+                    <div className='description w-75 ms-2'>
+                        <div className='d-flex justify-content-between align-items-center border-bottom'>
+                            <h3>{item.title}</h3>
+                            <span>{item.price}</span>
+                        </div>
+                            <p className='pt-1'>{item.description}</p>
                      </div>
                  </div>
             })}
