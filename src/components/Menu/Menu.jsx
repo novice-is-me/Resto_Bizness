08f28e137f5cb962e77d94329472180756a5cfe9
Regina Jalandoni <reginajalandoni13@gmail.com>
2024-02-10 17:53:36 +0800

    Updating MenuCard

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import MenuCard from '../MenuCard/MenuCard';
+import { breakfastData, lunchData, dinnerData} from '../MenuFile'
 import './menu.css'
 
 const Menu = () => {
@@ -7,13 +8,19 @@ const Menu = () => {
 
   return (
     <div className='menu m-0 p-5'>
-      <div className='container'>
-        <div>
+      <div className='container text-center'>
+        <div className='d-flex flex-column justify-content-center align-items-center'>
             <h3>Food Menu</h3>
             <h2>Most Popular Items</h2>
+            <div className='d-flex justify-content-evenly border border-2 w-50 choices'>
+              <a href="" className='nav-link p-2'>Breakfast</a>
+              <a href="" className='nav-link p-2'>Lunch</a>
+              <a href="" className='nav-link p-2'>Dinner</a>
+            </div>
         </div>
         <div className='choices'>
-          <MenuCard/>
+          {/* {show ?  : } */}
+          {/* <MenuCard data={}/> */}
         </div>
       </div>
     </div>
