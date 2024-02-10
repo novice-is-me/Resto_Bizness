b5782213fd92635ece446adbc8d1bdf9466c0326
Regina Jalandoni <reginajalandoni13@gmail.com>
2024-02-10 21:49:03 +0800

    Menu Responsiveness

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -12,18 +12,18 @@ const Menu = () => {
         <div className='d-flex flex-column justify-content-center align-items-center'>
             <h3>Food Menu</h3>
             <h2>Most Popular Items</h2>
-            <div className='d-flex justify-content-evenly border border-2 w-50 choices'>
-              <a href="" className='nav-link p-2'
+            <div className='d-flex justify-content-evenly w-50 choices'>
+              <a href="" className='nav-link p-2 breakfast'
                 onClick={(e) => {
                   e.preventDefault();
-                  setShow('default')
+                  setShow('default');
                 }}>Breakfast</a>
-              <a href="" className='nav-link p-2' 
+              <a href="" className='nav-link p-2 lunch' 
               onClick={(e) => {
                   e.preventDefault();
                   setShow('lunch')
                 }}>Lunch</a>
-              <a href="" className='nav-link p-2' 
+              <a href="" className='nav-link p-2 dinner' 
                 onClick={(e) => {
                   e.preventDefault();
                   setShow('dinner')
--- src/components/MenuCard/MenuCard.jsx
+++ src/components/MenuCard/MenuCard.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { breakfastData, lunchData, dinnerData} from '../MenuFile'
 import './menuCard.css'
 
 const MenuCard = ({data}) => {
+
   return (
     <div className='menuCard m-0 p-5'>
       <div className='row'>
             {data.map((item, index) => {
-                return <div className='col col-lg-6 col-md-12 d-flex align-items-center'> 
+                return <div className='col col-lg-6 col-12 d-flex align-items-center menu-container'> 
                      <div className='image w-25'>
                          <img src={item.img} alt="" className='img-fluid' />
                      </div>
-                    <div className='description w-75 ms-2'>
+                    <div className='description w-75 ms-2 text-start'>
                         <div className='d-flex justify-content-between align-items-center border-bottom'>
                             <h3>{item.title}</h3>
                             <span style={{color: "#FF5F00", fontWeight: "bold"}}>{item.price}</span>
